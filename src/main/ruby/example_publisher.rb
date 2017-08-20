97ba0021232483cd17f307fa522ed60641cb89e2
André Formento <andreformento.sc@gmail.com>
2017-08-20 11:12:45 -0300

    Remove duplicatad declaration

--- src/main/ruby/example_publisher.rb
+++ src/main/ruby/example_publisher.rb
@@ -13,17 +13,13 @@ ch = conn.create_channel
 
 #Declare a queue with a given name, examplequeue. In this example is a durable shared queue used.
 q  = ch.queue("examplequeue", :durable => true)
-# ch.queue is used to declare a queue with a particular name, in this case, the queue is called examplequeue. The queue in this example is marked as durable, meaning that RabbitMQ will never lose our queue.
 
-#BIND THE QUEUE TO AN EXCHANGE
 #For messages to be routed to queues, queues need to be bound to exchanges.
 x = ch.direct("example.exchange", :durable => true)
 
 #Bind a queue to an exchange
 q.bind(x, :routing_key => "process")
 
-#For messages to be routed to queues, queues need to be bound to exchanges.
-x = ch.direct("example.exchange", :durable => true)
 information_message = "{\"email\": \"example@mail.com\",\"name\": \"name\",\"size\": \"size\"}"
 
 x.publish(information_message,
