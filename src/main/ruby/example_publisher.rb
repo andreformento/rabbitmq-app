1e0820c86ad238292440a86e0aef036df0737502
André Formento <andreformento.sc@gmail.com>
2017-08-20 11:19:05 -0300

    Change to local connection

--- src/main/ruby/example_publisher.rb
+++ src/main/ruby/example_publisher.rb
@@ -4,7 +4,7 @@ require "bunny"
 require "json"
 
 #Returns a connection instance
-conn = Bunny.new ENV['CLOUDAMQP_URL']
+conn = Bunny.new(:host => "localhost")
 #The connection will be established when start is called
 conn.start
 
