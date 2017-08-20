74dce01c367f7943fa4e1e27b11ac31c3cb46154
André Formento <andreformento.sc@gmail.com>
2017-08-19 23:41:31 -0300

    Add ruby code

---
+++ src/main/ruby/example_publisher.rb
@@ -0,0 +1,30 @@
+#example_publisher.rb
+require "rubygems"
+require "bunny"
+require "json"
+
+#Returns a connection instance
+conn = Bunny.new ENV['CLOUDAMQP_URL']
+#The connection will be established when start is called
+conn.start
+
+#create a channel in the TCP connection
+ch = conn.create_channel
+
+#Declare a queue with a given name, examplequeue. In this example is a durable shared queue used.
+q  = ch.queue("examplequeue", :durable => true)
+
+#Bind a queue to an exchange
+q.bind(x, :routing_key => "process")
+
+#For messages to be routed to queues, queues need to be bound to exchanges.
+x = ch.direct("example.exchange", :durable => true)
+information_message = "{\"email\": \"example@mail.com\",\"name\": \"name\",\"size\": \"size\"}"
+
+x.publish(information_message,
+  :timestamp      => Time.now.to_i,
+  :routing_key    => "process"
+)
+
+sleep 1.0
+conn.close
