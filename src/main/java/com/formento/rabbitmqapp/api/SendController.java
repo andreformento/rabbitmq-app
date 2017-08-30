1128e81a92c4b2b6a2b3eb2afccc1a715b189ec8
André Formento <andreformento.sc@gmail.com>
2017-08-29 22:44:55 -0300

    Put log

--- src/main/java/com/formento/rabbitmqapp/api/SendController.java
+++ src/main/java/com/formento/rabbitmqapp/api/SendController.java
@@ -1,5 +1,9 @@
 package com.formento.rabbitmqapp.api;
 
+import static org.springframework.util.StringUtils.isEmpty;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,11 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.util.StringUtils.isEmpty;
-
 @RestController
 @RequestMapping("/queue")
 public class SendController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SendController.class);
 
     private final RabbitTemplate rabbitTemplate;
 
@@ -22,7 +26,9 @@ public class SendController {
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void send(final String message) {
-        rabbitTemplate.convertAndSend(ApiApplication.queueName, isEmpty(message) ? "Hello from RabbitMQ!" : message);
+        final String messageQueue = isEmpty(message) ? "Hello from RabbitMQ!" : message;
+        LOGGER.info("Put a message into rabbitmq <" + messageQueue + ">");
+        rabbitTemplate.convertAndSend(ApiApplication.queueName, messageQueue);
     }
 
 }
